49d5a04d205299433cb65bde474ae9b7d6fdf3a3
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-10-11 00:08:37 +0530

    first commit

---
+++ Remove_duplicates.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/remove-duplicate-letters/
+Input: s = "cbacdcbc"
+Output: "acdb"
+
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int>cnt(128,0);
+        vector<bool>vis(128,false);
+        string res="0";
+        for(int i=0;i<s.length();i++){
+            cnt[s[i]]++;
+        }
+        for(int i=0;i<s.length();i++){
+            cnt[s[i]]--;
+            if(vis[s[i]])continue;
+          
+            while(res.back()>s[i] && cnt[res.back()]){
+                vis[res.back()]=false;
+                res.pop_back();
+            }
+            res+=s[i];
+              vis[s[i]]=true;
+        }
+        return res.substr(1);
+    }
+};
